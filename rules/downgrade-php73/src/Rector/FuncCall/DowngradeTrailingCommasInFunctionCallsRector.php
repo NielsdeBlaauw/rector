db9a74aa63c02447164a90643bd46c30348cbe2e
TomasVotruba <tomas.vot@gmail.com>
2021-03-05 15:05:24 +0100

    [Downgrade 7.3] Downgrade trailing comma only if exists

--- rules/downgrade-php73/src/Rector/FuncCall/DowngradeTrailingCommasInFunctionCallsRector.php
+++ rules/downgrade-php73/src/Rector/FuncCall/DowngradeTrailingCommasInFunctionCallsRector.php
@@ -4,20 +4,34 @@ declare(strict_types=1);
 
 namespace Rector\DowngradePhp73\Rector\FuncCall;
 
+use Nette\Utils\Strings;
 use PhpParser\Node;
+use PhpParser\Node\Arg;
 use PhpParser\Node\Expr\FuncCall;
 use PhpParser\Node\Expr\MethodCall;
 use PhpParser\Node\Expr\StaticCall;
+use Rector\Core\Application\TokensByFilePathStorage;
 use Rector\Core\Rector\AbstractRector;
 use Rector\NodeTypeResolver\Node\AttributeKey;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
 use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+use Symplify\SmartFileSystem\SmartFileInfo;
 
 /**
  * @see \Rector\DowngradePhp73\Tests\Rector\FuncCall\DowngradeTrailingCommasInFunctionCallsRector\DowngradeTrailingCommasInFunctionCallsRectorTest
  */
 final class DowngradeTrailingCommasInFunctionCallsRector extends AbstractRector
 {
+    /**
+     * @var TokensByFilePathStorage
+     */
+    private $tokensByFilePathStorage;
+
+    public function __construct(TokensByFilePathStorage $tokensByFilePathStorage)
+    {
+        $this->tokensByFilePathStorage = $tokensByFilePathStorage;
+    }
+
     public function getRuleDefinition(): RuleDefinition
     {
         return new RuleDefinition(
@@ -66,11 +80,53 @@ CODE_SAMPLE
     public function refactor(Node $node): ?Node
     {
         if ($node->args) {
-            $node->setAttribute(AttributeKey::ORIGINAL_NODE, null);
-            $last = $node->args[array_key_last($node->args)];
+            $lastArgumentPosition = array_key_last($node->args);
+
+            $last = $node->args[$lastArgumentPosition];
+            if (! $this->isNodeFollowedByComma($last)) {
+                return null;
+            }
+
+            // remove comma
             $last->setAttribute(AttributeKey::FUNC_ARGS_TRAILING_COMMA, false);
+            $node->setAttribute(AttributeKey::ORIGINAL_NODE, null);
         }
 
         return $node;
+    }
+
+    private function isNodeFollowedByComma(Arg $arg): bool
+    {
+        $smartFileInfo = $arg->getAttribute(AttributeKey::FILE_INFO);
+        if (! $smartFileInfo instanceof SmartFileInfo) {
+            return false;
+        }
+
+        if (! $this->tokensByFilePathStorage->hasForFileInfo($smartFileInfo)) {
+            return false;
+        }
+
+        $parsedStmtsAndTokens = $this->tokensByFilePathStorage->getForFileInfo($smartFileInfo);
+        $oldTokens = $parsedStmtsAndTokens->getOldTokens();
+
+        $nextTokenPosition = $arg->getEndTokenPos() + 1;
+        while (isset($oldTokens[$nextTokenPosition])) {
+            $currentToken = $oldTokens[$nextTokenPosition];
+
+            // only space
+            if (is_array($currentToken) || Strings::match($currentToken, '#\s+#')) {
+                ++$nextTokenPosition;
+                continue;
+            }
+
+            // without comma
+            if ($currentToken === ')') {
+                return false;
+            }
+
+            break;
+        }
+
+        return true;
     }
 }
